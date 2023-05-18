2bfd121fb1c161ad6c7619bbbe12987ddebb5ac9
AdryanAlbuquerque <adryanfelipe17@hotmail.com>
2023-05-18 15:57:02 -0300

    atv1

--- classe/atv1.py
+++ classe/atv1.py
@@ -1,12 +1,13 @@
 class Pessoa:
-    def __init__(self,nome,peso,idade,comendo=False):
+    def __init__(self,nome,peso,idade,comendo=False,):
         self.nome=nome
         self.peso=peso
         self.idade=idade
         self.comendo=comendo
 
-    def comer(self):
-        print(f"{self.nome} está comendo")
+    def comer(self,alimento):
+        self.alimento=alimento
+        print(f"{self.nome} está comendo{self.alimento}")
 
 
 
@@ -14,7 +15,7 @@ class Pessoa:
 p1 = Pessoa("joão",80,19)
 p2 = Pessoa("Marta",56,22,comendo=True)
 
-p2.comer()
+p2.comer(" pipoca")
 
 
 #print(vars(p2))
