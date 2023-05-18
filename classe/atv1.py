ed30794b44d3bfe0c6051aee65e905a0f37ab264
AdryanAlbuquerque <adryanfelipe17@hotmail.com>
2023-05-18 15:45:53 -0300

    atv1

--- classe/atv1.py
+++ classe/atv1.py
@@ -1,5 +1,11 @@
 class Pessoa:
-    def __init__(self,nome,peso,idade,comando=False):
+    def __init__(self,nome,peso,idade,comendo=False):
         self.nome=nome
         self.peso=peso
         self.idade=idade
+        self.comendo=comendo
+
+p1 = Pessoa("joão",80,19)
+p2 = Pessoa("Marta",56,22,comendo=True)
+print(vars(p2))
+print(vars(p1))
